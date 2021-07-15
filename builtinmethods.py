cebaaf742886a39f0cbd263e7fe092c12cab2f02
Derrickragui <mwangiiwambuguu@gmail.com>
2021-07-15 17:22:45 +0300

    First commit

---
+++ builtinmethods.py
@@ -0,0 +1,5 @@
+Students = [{"Name":"Derrick","Age": 19 , "Gender":"Male"},{"Name":"Keith", "Age":24 ,"Gender": "Male"},{"Name":"Tia","Age": 19 ,"Gender":"Female"},{"Name":"Dossy", "Age":19 ,"Gender": "Male"}]
+for student in Students:
+    print(student['Name']+" " +str(student['Age'])+ " ",student['Gender'])
+
+    
